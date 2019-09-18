bb88cb95d56ee0cba8511e15d22fcefc93dab545
Mohit <sopranosmohit@gmail.com>
2019-09-18 21:44:38 +0530

    Checking in the project with WebClientBuilder

--- src/main/java/io/mylearning/moviecatalogservice/MovieCatalogResource.java
+++ src/main/java/io/mylearning/moviecatalogservice/MovieCatalogResource.java
@@ -1,26 +1,45 @@
 package io.mylearning.moviecatalogservice;
 
-import io.mylearning.moviecatalogservice.CatalogItem;
+
+import io.mylearning.moviecatalogservice.models.Movie;
 import io.mylearning.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping(value = "/{userId}",method = RequestMethod.GET)
     public List<CatalogItem> getCatalogItemList(@PathVariable String userId)
     {
-        List<Rating> ratings = Arrays.asList(new Rating("1234",4),new Rating("5678",5));
-        return Collections.singletonList(new CatalogItem("Tansformers","Test",4));
+
+
+        RestTemplate template = new RestTemplate();
+
+
+        List<Rating> ratingsList = Arrays.asList(new Rating("1234",4),new Rating("5678",5));
+
+        return ratingsList.stream()
+                .map(rating -> {
+                    Movie movie = template.getForObject("http://localhost:8080/movie/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+       
 
 
     }
