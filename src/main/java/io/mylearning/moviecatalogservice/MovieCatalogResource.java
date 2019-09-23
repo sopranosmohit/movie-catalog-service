f7c4aca3e3edda68479f64048b31f2dc0c570ba4
Mohit <sopranosmohit@gmail.com>
2019-09-23 23:01:22 +0530

    Checking in the project with Hysterix Enabled as FallBack

--- src/main/java/io/mylearning/moviecatalogservice/MovieCatalogResource.java
+++ src/main/java/io/mylearning/moviecatalogservice/MovieCatalogResource.java
@@ -1,15 +1,18 @@
 package io.mylearning.moviecatalogservice;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import io.mylearning.moviecatalogservice.models.Movie;
 import io.mylearning.moviecatalogservice.models.Rating;
+import io.mylearning.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,28 +22,28 @@ import java.util.stream.Collectors;
 @RequestMapping(path = "/catalog")
 public class MovieCatalogResource {
 
+
     @Autowired
-    private WebClient.Builder webClientBuilder;
+    private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/{userId}",method = RequestMethod.GET)
-    public List<CatalogItem> getCatalogItemList(@PathVariable String userId)
-    {
+    @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod ="getFallBackCatalog" )
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
+        UserRating userRating = restTemplate.getForObject("http://rating-info-service/ratingsdata/user/" + userId, UserRating.class);
 
-        RestTemplate template = new RestTemplate();
-
-
-        List<Rating> ratingsList = Arrays.asList(new Rating("1234",4),new Rating("5678",5));
-
-        return ratingsList.stream()
+        return userRating.getRatings().stream()
                 .map(rating -> {
-                    Movie movie = template.getForObject("http://localhost:8080/movie/" + rating.getMovieId(), Movie.class);
-                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
-                })
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movie/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());                })
                 .collect(Collectors.toList());
 
-       
-
-
     }
+
+
+    public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId)
+    {
+        return Arrays.asList(new CatalogItem("Dummy","Dummy Description",0));
+    }
+
 }
--- src/main/java/io/mylearning/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/io/mylearning/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,20 +2,28 @@ package io.mylearning.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+@ComponentScan
 public class MovieCatalogServiceApplication {
 
-    @Bean
-    public WebClient.Builder getWebClientBuilder() {
-       return  WebClient.builder();
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(MovieCatalogServiceApplication.class, args);
     }
 
-
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
